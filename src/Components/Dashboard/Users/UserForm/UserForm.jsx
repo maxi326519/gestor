00a5bf8d84cf3a7b4cd095a2bd05612ba447b8b4
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2023-01-24 00:07:04 -0300

    Update

--- src/Components/Dashboard/Users/UserForm/UserForm.jsx
+++ src/Components/Dashboard/Users/UserForm/UserForm.jsx
@@ -1,36 +1,60 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../../../../redux/actions';
+
 import './UserForm.css'
 
 export default function UserForm(){
+
+    const dispatch = useDispatch();
+    const [user, setUser] = useState({
+        name: "",
+        userName: "",
+        email: "",
+        adress: "",
+        phone: ""
+    });
+
+    function handleChange(e){
+        console.log(user);
+        setUser({ ...user, [e.target.name]: e.target.value});
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+        dispatch(addUser(user));
+    }
+
     return(
-        <form className="userForm" >
+        <form className="userForm" onSubmit={handleSubmit}>
             {/* Name */}
             <div className="mb-3">
                 <label className="form-label">Nombre</label>
-                <input className="form-control"/>
+                <input className="form-control" name="name" onChange={handleChange}/>
             </div>
 
             {/* Usuario */}
             <div className="mb-3">
                 <label className="form-label">Usuario</label>
-                <input className="form-control"/>
+                <input className="form-control" name="userName" onChange={handleChange}/>
             </div>
 
             {/* Email*/}
             <div className="mb-3">
                 <label className="form-label">Email</label>
-                <input className="form-control"/>
+                <input className="form-control" name="email" onChange={handleChange}/>
             </div>
 
             {/* Adress */}
             <div className="mb-3">
                 <label className="form-label">Direccion</label>
-                <input className="form-control"/>
+                <input className="form-control" name="adress" onChange={handleChange}/>
             </div>
 
             {/* Phone */}
             <div className="mb-3">
                 <label className="form-label">Telefono</label>
-                <input className="form-control"/>
+                <input className="form-control" name="phone" onChange={handleChange}/>
             </div>
 
             <button type='submit' className='btn btn-success'>
--- src/Components/Dashboard/Users/UserList/UserList.jsx
+++ src/Components/Dashboard/Users/UserList/UserList.jsx
@@ -1,27 +1,24 @@
 import React from "react";
 import { useSelector } from "react-redux";
-
-import UserCard from "./UserCard/UserCard";
+import DataGrid from 'react-data-grid';
+import 'react-data-grid/lib/styles.css';
 
 import "./UserList.css";
 
 export default function UserList() {
-   const users = useSelector((state) => state.users.data);
+  const rows = useSelector((state) => state.users.data);
+  const columns = [
+    { key: "name", name: "Nombre"},
+    { key: "userName", name: "Usuario"},
+    { key: "email", name: "Email"},
+    { key: "adress", name: "Direccion"},
+    { key: "phone", name: "Telefono"},
+  ]
 
   return (
     <div className="userList">
       <h3>Listado de Usuarios</h3>
-      {
-        users?.map( m =>
-          <UserCard
-            name={m.name}
-            userName={m.userName}
-            email={m.email}
-            adress={m.adress}
-            phone={m.phone}
-          />
-        )
-      }
+      <DataGrid columns={columns} rows={rows}/>
     </div>
   );
 }
